8fcc5263da8d51cc68099cc2cb4f63d3ec4f2a65
Atik Sahariyar <atiksahariyar88@gmail.com>
2023-11-30 06:19:19 +0600

    my existing project

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:5000"
-    // baseURL: "https://friend-fusion-server.vercel.app"
+    // baseURL: "http://localhost:5000"
+    baseURL: "https://friend-fusion-server.vercel.app"
 })
+
 const useAxiosPublic = () => {
 
     return axiosPublic
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -18,23 +18,3 @@ const app = initializeApp(firebaseConfig);
 
 export default app
 
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAfx7IyPh-s1NKNhSK5Z8T8DYmYvhM_xsM",
-//   authDomain: "friend-fusion-22533.firebaseapp.com",
-//   projectId: "friend-fusion-22533",
-//   storageBucket: "friend-fusion-22533.appspot.com",
-//   messagingSenderId: "213406418819",
-//   appId: "1:213406418819:web:f97c0f8117edfdb67d18b6",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export default app
